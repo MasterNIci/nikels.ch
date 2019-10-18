abea5b03aaaf6b6120fbebcef971902251d65c2a
MasterNici <54352264+MasterNIci@users.noreply.github.com>
2019-10-18 13:09:01 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 150,
+        "value": 100,
         "density": {
           "enable": true,
           "value_area": 800
