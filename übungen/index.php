6371a9a871d666eb7087fe077324609c9ca6b399
MasterNici <54352264+MasterNIci@users.noreply.github.com>
2019-11-04 12:22:22 +0100

    Create index.php

---
+++ übungen/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="de_ch" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo "hallo" ?>
+  </body>
+</html>
